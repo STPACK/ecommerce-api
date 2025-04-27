eeb905753034a5cd90c0b5c7829dd08f1c19ec93
stpack <sattha.prat@hotmail.com>
2025-04-27 19:24:11 +0700

    feat: configure TypeORM and set up Swagger for API documentation

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,20 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ProductsModule } from './modules/products/products.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'password',
+      database: 'ecommerce',
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true, // สำหรับ dev เท่านั้น (production ไม่ควรเปิด)
+    }),
+    ProductsModule,
+  ],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create(AppModule,{
+    rawBody: true,
+  });
+
+  const config = new DocumentBuilder()
+    .setTitle('E-Commerce API')
+    .setDescription('The e-commerce API documentation')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  await app.listen(8081);
 }
 bootstrap();
